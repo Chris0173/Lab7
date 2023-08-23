af25848f657a34cb96a3d3631fb2f0e074068635
chris0173 <christopherlacock@gmail.com>
2023-08-23 15:07:05 +1200

    Login Page w/ basic styling

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './Login.module.css';
+import { useNavigate } from 'react-router-dom'
 
-const LoginPage = () => {
-  return <div>Login Page: Please log in to continue.</div>;
+const Login = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const dummyUser = {
+    username: 'admin',
+    password: 'adminPassword'
+  };
+
+  const handleSignIn = () => {
+    if (!username || !password) {
+      alert('Please enter username and password');
+      return;
+    }
+
+    if (username === dummyUser.username && password === dummyUser.password) {
+      navigate('/');
+    } else { 
+      alert('Invaild username or password');
+    }
+  };
+
+  return (
+    <div className={styles['login-container']}>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input type="text" className={styles['form-control']} value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" className={styles['form-control']} value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button type="button" onClick={handleSignIn}>
+          Sign In
+        </button>
+        <p>
+          <a href="#">Forgot Password?</a>
+        </p>
+      </form>
+    </div>
+  );
 };
 
-export default LoginPage;
+export default Login;
